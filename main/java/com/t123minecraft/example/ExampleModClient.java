8fab654b71c7abe1709ca84bd732d2cde35ee742
T123Minecraft <158288816+T123MINECRAFT@users.noreply.github.com>
2025-09-13 23:40:28 +0800

    Add files via upload

---
+++ main/java/com/t123minecraft/example/ExampleModClient.java
@@ -0,0 +1,9 @@
+package com.t123minecraft.example;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExampleModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
